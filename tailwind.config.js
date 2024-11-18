83b4d3a160b857f728bb2c47d7442d4b789fef9a
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2024-11-18 09:56:36 +0100

    Branding and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,23 +6,22 @@ module.exports = {
   theme: {
     extend: {
       textColor: {
-        primary: '#1D4ED8',
-        success: '#25AD25',
-        secondary: '#F0AC27',
-        error: '#DC2626',
+        primary: '#1D2B31',
+        success: '#218838',
+        secondary: '#F5A623',
+        error: '#FF4040',
       },
       backgroundColor: {
-        primary: '#1D4ED8',
-        secondary: '#F0AC27',
-        error: '#DC2626',
-        grey: '#DEDCDC',
+        primary: '#1D2B31',
+        secondary: '#F5A623',
+        error: '#FF4040',
       },
       borderColor: {
         primary: '#D0D5DD',
       },
       fontFamily: {
-        primary: 'Inter, sans-serif',
-        secondary: 'Plus Jakarta Sans, sans-serif',
+        primary: 'Montserrat, sans-serif',
+        secondary: 'Inter, sans-serif',
       },
       padding: {
         primary: '5vw',
