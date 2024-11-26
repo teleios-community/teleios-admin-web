87c89a83c2d694ac1ae320e32f27476967abde51
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2024-11-26 08:38:51 +0100

    cleared files directory to fix casing issue

--- src/routes/index.tsx
+++
@@ -1,7 +0,0 @@
-import { RouteObject } from 'react-router-dom';
-import authRoutes from './auth';
-import dashboardRoutes from './dashboard';
-
-const routes: RouteObject[] = [...authRoutes, ...dashboardRoutes];
-
-export default routes;
--- src/types/index.ts
+++
@@ -1,3 +0,0 @@
-export type UncertainObjectType = {
-  [key: string]: unknown;
-};
